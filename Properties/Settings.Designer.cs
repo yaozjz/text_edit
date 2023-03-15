73f163e1d00af0f317f086728449321769b4b561
yaozjz <logiczero@126.com>
2023-03-15 16:46:42 +0800

    新增功能

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace textEdit.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("(.*?)[\\.\\\'\\?。？”’！」：…＊）]$")]
+        [global::System.Configuration.DefaultSettingValueAttribute("(.*?)[\\.\\\'\\?。？”’！」：…＊）】]$")]
         public string DuanWei {
             get {
                 return ((string)(this["DuanWei"]));
